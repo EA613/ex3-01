2a5a7f8e1e359d2133c183c06cf7d0515a5b1c12
EA613 <256419728+EA613@users.noreply.github.com>
2026-06-15 18:53:19 -0400

    Changed Hello World message for a 2nd time.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World, MoocyDocker613 here!  This a test of ex3.1 of the DevOps with Docker course.</h1>')
+  res.send('<h1>Hello World!  This a test of ex3.1 of the DevOps with Docker course.</h1>')
+  res.send("<h2>Hugs and Kisses, EA613 (aka MoocyDocker613 on the streets of ol' Dockertowne)</h2>")
 })
 
 const PORT = 8080
