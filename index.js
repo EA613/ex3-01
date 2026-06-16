bccb375da83fd8f890fd0d4090cc9f61b20b789e
EA613 <256419728+EA613@users.noreply.github.com>
2026-06-15 23:47:25 -0400

    Added Renderville reference to index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send("<h1>Hello World!  This a test of ex3.1 of the DevOps with Docker course.</h1><h2>With love and squalor,<br /> EA613 (aka MoocyDocker613 on the streets of ol' Dockertowne)</h2>")
+  res.send("<h1>Hello World!  This a test of ex3.1 of the DevOps with Docker course.</h1><h2>With love and squalor,<br /> EA613 (aka MoocyDocker613 on the streets of ol' Dockertowne and EA613-Render in the shantytowns of Renderville)</h2>")
 })
 
 const PORT = 8080
